6972ad9745e3d2c05182bbc54fc532945f5f7902
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-01-05 21:04:01 +0200

    Modify bootloader entry to use labels and add symlinker script

---
+++ installation_scripts/helper_scripts/create_symlinks.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo "=== INFO ==="
+echo "This script creates all necessary symlinks from and to ~/.config"
+echo "// Symlinking zshenv to HOME and sourcing it"
+dest=$HOME/.zshenv
+[[ -e $dest ]] || ln -s $HOME/.config/.zshenv $HOME && source $dest
+echo "Done"
+
+echo "// Symlinking font configs from /etc/fonts/conf.avail/"
+destfolder=$XDG_CONFIG_HOME/fontconfig/conf.d
+configlist=$destfolder/fontconfigs
+while read F; do
+    # Parse comments out
+    if [[ ${F:0:1} != '#' ]]; then
+        [[ -e $destfolder/$F ]] || ln -s /etc/fonts/conf.avail/$F $destfolder/
+    fi
+done < $configlist
+echo "Done"
+
+echo "// Symlinking fonts folder to XDG_DATA_HOME"
+dest=$XDG_DATA_HOME/fonts
+[[ -e $dest ]] || ln -s $XDG_CONFIG_HOME/fonts $XDG_DATA_HOME/
+echo "Done"
+
+echo "// Symlinking gnupg config to GNUPGHOME"
+dest=$GNUPGHOME/gpg-agent.conf
+[[ -e $dest ]] || ln -s $XDG_CONFIG_HOME/gnupg/gpg-agent.conf $GNUPGHOME/
+echo "Done"
+
+echo "// Symlinking pacman hooks to /usr/share/libalpm/hooks/"
+destfolder=/usr/share/libalpm/hooks
+for f in $XDG_CONFIG_HOME/pacman-hooks/*.hook; do
+    name=$(echo $f | xargs -n 1 basename)
+    [[ -e $destfolder/$name ]] || sudo ln -s $f $destfolder/
+done
+echo "Done"
