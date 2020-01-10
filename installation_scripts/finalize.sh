38ac0a33ab834cec7d5ee7d3b11af394d593bccb
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-01-10 19:16:51 +0200

    Change udev rules to symlinks instead of rsync copies

--- installation_scripts/finalize.sh
+++ installation_scripts/finalize.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 echo "// Creating folders"
-[[ -e $GNUPGHOME ]] || mkdir -p $GNUPGHOME && chmod 700 $GNUPGHOME
 [[ -e $XDG_DATA_HOME/nvim/backup ]] || mkdir -p $XDG_DATA_HOME/nvim/backup
+[[ -e $XDG_DATA_HOME/zsh ]] || mkdir $XDG_DATA_HOME/zsh
 
 echo "// Symlinking fonts folder to XDG_DATA_HOME"
 dest=$XDG_DATA_HOME/fonts
 [[ -e $dest ]] || ln -s $XDG_CONFIG_HOME/fonts $XDG_DATA_HOME/
-echo "Done"
-
-echo "// Symlinking gnupg config to GNUPGHOME"
-dest=$GNUPGHOME/gpg-agent.conf
-[[ -e $dest ]] || ln -s $XDG_CONFIG_HOME/gnupg/gpg-agent.conf $GNUPGHOME/
 echo "Done"
 
 echo "// Symlinking pacman hooks to /usr/share/libalpm/hooks/"
@@ -19,4 +14,17 @@ for f in $XDG_CONFIG_HOME/pacman-hooks/*.hook; do
     name=$(echo $f | xargs -n 1 basename)
     [[ -e $destfolder/$name ]] || sudo ln -s $f $destfolder/
 done
+echo "Done"
+
+echo "// Symlinking udev hwdb-maps to /etc/udev/hwdb.d/"
+destfolder=/etc/udev/hwdb.d/
+for f in /home/otahontas/.config/etc/udev/hwdb.d/*; do
+    name=$(echo $f | xargs -n 1 basename)
+    [[ -e $destfolder/$name ]] || sudo ln -s $f $destfolder/
+done
+echo "Done"
+
+echo "// Reloading hwdb for new keymap"
+sudo udevadm hwdb --update
+sudo udevadm trigger
 echo "Done"
