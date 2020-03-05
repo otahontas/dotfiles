ff025d1faa6effd90ba289c1e2562924d1b608bc
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-03-05 12:51:51 +0200

    Modify some zsh settings

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,5 +1,5 @@
 # Set FZF defaults
 export FZF_DEFAULT_OPTS='--height 50% --layout=reverse'
-export FZF_DEFAULT_COMMAND='fd -H'
-export FZF_ALT_C_COMMAND='fd -H -t d'
-export FZF_CTRL_T_COMMAND='fd -H -t f'
+export FZF_DEFAULT_COMMAND='fd -H --follow --exclude .git'
+export FZF_ALT_C_COMMAND='fd -H --follow --exclude .git -t d'
+export FZF_CTRL_T_COMMAND='fd -H --follow --exclude .git -t f'
