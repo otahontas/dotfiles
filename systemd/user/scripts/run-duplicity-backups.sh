33b7f20dcaa8750067f4478a4e638674dfdb2169
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-03-27 11:13:09 +0200

    Add new duplicity backup system with gpg encryption and failure/success notifications

--- systemd/user/scripts/run-duplicity-backups.sh
+++ systemd/user/scripts/run-duplicity-backups.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-duplicity /etc file:///media/backup/etc --no-encryption
-duplicity --exclude /home/otahontas.cache /home/otahontas file:///media/backup/home --no-encryption
+backupdisk=/run/media/otahontas/archis_backup/
+if [[ -e $backupdisk ]]; then
+    notify-send "Backing up /home and /etc" "gpg-agent authentication possibly needed"
+    # Pinentry-qt needs DISPLAY env var in order to be able to ask for password
+    key=$(env DISPLAY=":0" pass keys/archis_backup)
+    PASSPHRASE=$key duplicity /etc file://$backupdisk/etc
+    etc_backup_exit_status=$?
+    PASSPHRASE=$key duplicity --exclude /home/otahontas/.cache /home/otahontas/ file://$backupdisk/home
+    home_backup_exit_status=$?
+    if [[ $etc_backup_exit_status -ne 0 || $home_backup_exit_status -ne 0 ]]; then
+        notify-send "Error happened while doing backup" "Backing up either /home, /etc or both failed"
+        exit 1;
+    fi
+else
+    notify-send "Disk wasn't connected, didn't perform backup"
+    exit 1;
+fi
+notify-send "Successfully backed up /home and /etc to external disk"
+exit 0;
