585ea7d674d9e2bfcbfb384091ce76d0784796c0
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-03-27 11:14:32 +0200

    Add notifications to bootloader settings backup

--- systemd/user/scripts/backup-bootloader-settings.sh
+++ systemd/user/scripts/backup-bootloader-settings.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 /usr/bin/rsync -a /boot/loader $XDG_CONFIG_HOME/boot
+exit_status=$?
+if [[ $exit_status -ne 0 ]]; then
+    exit 1;
+fi
+notify-send "Successfully backed up bootloader settings"
+exit 0;
