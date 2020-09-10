8cae8479a37c7634e1bea735cb602186459bce8b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-10 12:54:03 +0300

    Update battery check script, add redshift fix

--- dot_config/systemd/user/scripts/executable_hibernate-when-battery-low.sh
+++ dot_config/systemd/user/scripts/executable_hibernate-when-battery-low.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-acpi -b | grep "Battery 0" | awk -F'[,:%]' '{print $2, $3}' | {
-	read -r status capacity
-    if [ "$status" = Discharging -a "$capacity" -lt 10 ]; then
-		logger "Critical battery threshold"
+acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
+	read -r status1 capacity1
+    read -r status2 capacity2
+    if [ "$status1" = Discharging -o "$status2" = Discharging ] && [ "$capacity1" -lt 10 -a "$capacity2" -lt 10 ]; then
         notify-send "Critical battery threshold: battery under 10%" "Hibernating in 10 seconds"
+		logger "Critical battery threshold"
         sleep 10
-		systemctl hibernate
-    else 
-        if [ "$status" = Discharging -a "$capacity" -lt 15 ]; then
-            notify-send "Low battery: battery under 15%" "System will hibernate soon"
-        fi
+	    systemctl hibernate
+    elif [ "$status1" = Discharging -o "$status2" = Discharging ] && [ "$capacity1" -lt 15 -a "$capacity2" -lt 15 ]; then
+        notify-send "Low battery: battery under 15%" "System will hibernate soon"
 	fi
 }
