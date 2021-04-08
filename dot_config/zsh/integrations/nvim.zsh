8286217cf5be675f1bb3b9c90d470b910a5928be
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-04-08 22:13:54 +0300

    Add more pyenv and nvim configs to zsh

--- dot_config/zsh/integrations/nvim.zsh
+++ dot_config/zsh/integrations/nvim.zsh
@@ -1,6 +1,12 @@
 # Avoid nvim nesting
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
     if [ -x "$(command -v nvr)" ]; then
-        alias nvim=nvr
+        alias nvim="nvr -cc split --remote-wait +'set bufhidden=wipe'"
+        alias vim="nvr -cc split --remote-wait +'set bufhidden=wipe'"
+        alias vi="nvr -cc split --remote-wait +'set bufhidden=wipe'"
+    else 
+        alias nvim="echo 'No nesting!'"
+        alias vim="echo 'No nesting!'"
+        alias vi="echo 'No nesting!'"
     fi
 fi
