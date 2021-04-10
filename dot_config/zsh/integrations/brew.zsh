36fafc9f3cf52f391f3110681d6c02c34ddda633
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-04-10 12:00:50 +0300

    Update brew to use integrated brew bundle instead of brew-file

--- dot_config/zsh/integrations/brew.zsh
+++ dot_config/zsh/integrations/brew.zsh
@@ -1,10 +1,15 @@
 # Add brew sbin to path
 [[ $PATH != *"/usr/local/sbin"* ]] && export PATH=/usr/local/sbin:$PATH
 
-# Use brew wrapper to update Brewfile 
-if [ -f $(brew --prefix)/etc/brew-wrap ];then
-  source $(brew --prefix)/etc/brew-wrap
-fi
+# Wrapper for brew to execute brewfile dumping on certain brew commands
+function brew () {
+    local dump_commands=("install" "uninstall" "tap" "untap")
+    local main_command="${1}"
+    local brewfile_path="$(chezmoi source-path)/mac/packages/Brewfile"
 
-# Add homebrew file directly to chezmoi
-export HOMEBREW_BREWFILE=$(chezmoi source-path)/mac/packages/Brewfile
+    command brew ${@}
+
+    for command in "${dump_commands[@]}"; do
+        [[ "${command}" == "${main_command}" ]] && brew bundle dump --file="$brewfile_path" --force
+    done
+}
