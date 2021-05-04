ccf2480b2691b6b2eaa97af92bb7fbca763119b2
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-04 14:52:38 +0300

    typescript stuff

--- dot_config/nvim/lua/pluginsettings/formatter.lua
+++ dot_config/nvim/lua/pluginsettings/formatter.lua
@@ -12,7 +12,7 @@ local black = function() return {exe = "black", args = {"-"}, stdin = true} end
 
 local prettier = function()
     return {
-        exe = "npx prettier",
+        exe = "npm prettier",
         args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
         stdin = true
     }
