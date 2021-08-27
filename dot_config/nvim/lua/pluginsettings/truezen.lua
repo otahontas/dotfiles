68abddea25c143df2c52157bf2c8a6be48194797
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-27 09:21:59 +0300

    moar updates

---
+++ dot_config/nvim/lua/pluginsettings/truezen.lua
@@ -0,0 +1,14 @@
+--- Override some detault settings
+local zen = require("true-zen")
+local settings = {
+    ataraxis = {
+        force_when_plus_one_window = true,
+        left_padding = 20,
+        right_padding = 20
+    }
+}
+zen.setup(settings)
+
+-- Setup keybinding
+local map = require("utils").map
+map("n", "<leader>tz", ":TZAtaraxis")
