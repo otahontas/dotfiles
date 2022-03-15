8ce9662e755773ea9f3b5f4ccd21549e6dd026e5
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-03-15 21:06:10 +0200

    Fix root resolving for eslint and prettier

--- dot_config/nvim/lua/pluginsettings/null-ls.lua
+++ dot_config/nvim/lua/pluginsettings/null-ls.lua
@@ -7,85 +7,82 @@ local requires = {
 
 local config = function()
   local null_ls = require("null-ls")
+
+  local prettier_files = {
+    ".prettierrc*",
+    "package.json",
+  }
+
+  local prettier_condition = function(utils)
+    return utils.has_file(prettier_files) or utils.root_has_file(prettier_files)
+  end
+
+  local prettier_cwd = function(params)
+    local utils = require("null-ls.utils")
+    return utils.root_pattern(prettier_files)(params.bufname)
+  end
+
+  local eslint_files = {
+    ".eslintrc*",
+    "package.json",
+  }
+
+  local eslint_condition = function(utils)
+    return utils.has_file(eslint_files) or utils.root_has_file(eslint_files)
+  end
+
+  local eslint_cwd = function(params)
+    local utils = require("null-ls.utils")
+    return utils.root_pattern(eslint_files)(params.bufname)
+  end
+
   local options = {
+    debug = true,
     sources = {
-      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.code_actions.eslint_d.with({
+        cwd = eslint_cwd,
+        condition = eslint_condition,
+      }),
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.code_actions.shellcheck,
+      null_ls.builtins.diagnostics.actionlint,
+      null_ls.builtins.diagnostics.eslint_d.with({
+        cwd = eslint_cwd,
+        condition = eslint_condition,
+      }),
+      null_ls.builtins.diagnostics.hadolint,
+      null_ls.builtins.diagnostics.markdownlint,
+      null_ls.builtins.diagnostics.shellcheck.with({
+        diagnostics_format = "#{m} [#{s}] [#{c}]",
+      }),
+      null_ls.builtins.diagnostics.zsh,
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.eslint_d.with({
+        cwd = eslint_cwd,
+        condition = eslint_condition,
+      }),
+      null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.formatting.pg_format,
       null_ls.builtins.formatting.prettierd.with({
         env = {
           PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/prettierrc.json"),
         },
+        cwd = prettier_cwd,
+        condition = prettier_condition,
       }),
-      null_ls.builtins.formatting.eslint_d,
-      null_ls.builtins.diagnostics.eslint_d,
-      null_ls.builtins.code_actions.eslint_d,
-      null_ls.builtins.code_actions.gitsigns,
-      null_ls.builtins.code_actions.shellcheck,
-      null_ls.builtins.diagnostics.shellcheck.with({
-        diagnostics_format = "#{m} [#{s}] [#{c}]",
-      }),
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.trim_whitespace,
     },
-    update_on_insert = true,
-    on_attach = function(client, bufnr)
-      local mode = "n"
-      local opts = { noremap = true, silent = true }
-      local mappings = {
-        { "K", "<cmd>lua vim.lsp.buf.hover()<CR>" },
-        { "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>" },
-        {
-          "gi",
-          "<cmd>lua vim.lsp.buf.implementation()<CR>",
-        },
-        {
-          "gs",
-          "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-        },
-        {
-
-          "<leader>wa",
-          "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",
-        },
-        {
-
-          "<leader>wr",
-          "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",
-        },
-        {
-          "<leader>wl",
-          "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-        },
-        {
-          "<leader>D",
-          "<cmd>lua vim.lsp.buf.type_definition()<CR>",
-        },
-        { "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>" },
-        { "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>" },
-        { "<leader>ge", "<cmd>lua vim.diagnostic.setloclist()<CR>" },
-        { "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>" },
-        { "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>" },
-      }
-
-      for _, value in pairs(mappings) do
-        local key, command = unpack(value)
-        vim.api.nvim_buf_set_keymap(bufnr, mode, key, command, opts)
-      end
-
-      -- set up autoformatting
+    on_attach = function(client)
       vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
-
-      -- set up root dir
-      if client and client.config and client.config.root_dir then
-        vim.api.nvim_set_current_dir(client.config.root_dir)
-      end
+        augroup LspFormatting
+            autocmd! * <buffer>
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+        augroup END
+      ]])
 
       client.resolved_capabilities.document_formatting = true
       client.resolved_capabilities.document_range_formatting = true
-
-      require("illuminate").on_attach(client)
     end,
   }
 
