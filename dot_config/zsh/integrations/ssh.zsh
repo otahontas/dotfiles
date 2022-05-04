76d311ace8ced5fc428d0610d26a26a587f93cff
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-05-05 01:54:52 +0300

    Use 1password as ssh agent

--- dot_config/zsh/integrations/ssh.zsh
+++ dot_config/zsh/integrations/ssh.zsh
@@ -1,3 +1,6 @@
+# Use 1password as ssh agent
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 # Add SSH hostname completions from ~/.ssh/config
 h=()
 if [[ -r ~/.ssh/config ]]; then
