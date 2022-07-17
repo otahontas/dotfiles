0781b869287aaa7b97458fb08187000ddf579518
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-07-17 21:56:11 +0300

    Drop nvr

--- dot_config/nvim/lua/integrations/init.lua
+++ dot_config/nvim/lua/integrations/init.lua
@@ -1,2 +1,1 @@
 require("integrations.chezmoi")
-require("integrations.nvr")
--- dot_config/zsh/integrations/nvim.zsh
+++ dot_config/zsh/integrations/nvim.zsh
@@ -1,12 +1,3 @@
-# Avoid nvim nesting
-if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-    if [ -x "$(command -v nvr)" ]; then
-        alias nvim="env TERM=wezterm nvr -cc split --remote-wait +'set bufhidden=wipe'"
-    else
-        alias nvim="echo 'Nesting instances not allowed!'"
-    fi
-fi
-
 # Nvim needs to know wezterm TERM is set to enable various font styles
 alias nvim="env TERM=wezterm command nvim"
 
