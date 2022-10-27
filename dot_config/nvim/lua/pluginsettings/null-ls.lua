c2517ea25864d226f2dc4785329129e42e74b3be
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-10-27 22:32:53 +0200

    fiskes

--- dot_config/nvim/lua/pluginsettings/null-ls.lua
+++ dot_config/nvim/lua/pluginsettings/null-ls.lua
@@ -62,6 +62,9 @@ local config = function()
   local options = {
     debug = false,
     sources = {
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.code_actions.refactoring,
+      null_ls.builtins.code_actions.shellcheck,
       null_ls.builtins.diagnostics.actionlint,
       null_ls.builtins.diagnostics.cfn_lint,
       null_ls.builtins.diagnostics.gitlint,
