9ca9713a7b4d54c140ff89f8578a01936bcd1dbf
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-11-09 10:35:52 +0200

    Add cspell

--- dot_config/nvim/lua/pluginsettings/null-ls.lua
+++ dot_config/nvim/lua/pluginsettings/null-ls.lua
@@ -62,11 +62,13 @@ local config = function()
   local options = {
     debug = false,
     sources = {
+      null_ls.builtins.code_actions.cspell,
       null_ls.builtins.code_actions.gitsigns,
       null_ls.builtins.code_actions.refactoring,
       null_ls.builtins.code_actions.shellcheck,
       null_ls.builtins.diagnostics.actionlint,
       null_ls.builtins.diagnostics.cfn_lint,
+      null_ls.builtins.diagnostics.cspell,
       null_ls.builtins.diagnostics.gitlint,
       null_ls.builtins.diagnostics.golangci_lint,
       null_ls.builtins.diagnostics.hadolint,
