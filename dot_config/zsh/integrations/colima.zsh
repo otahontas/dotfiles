09f44b729eb31476979d162e7cdb70a47135562f
Otto Ahoniemi <otto@ottoahoniemi.fi>
2023-03-26 00:56:44 +0200

    Add better setup for colima

--- dot_config/zsh/integrations/colima.zsh
+++ dot_config/zsh/integrations/colima.zsh
@@ -1,1 +1,5 @@
 export DOCKER_HOST="unix://$HOME/.colima/default/docker.sock"
+
+# From https://github.com/testcontainers/testcontainers-java/issues/5034#issuecomment-1319812252
+export TESTCONTAINERS_HOST_OVERRIDE=$(colima ls -j | jq -r '.address')
+export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
